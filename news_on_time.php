a3ea569901e60dd55c96add966844cf1163bffd8
Nanette Taylor <nanette.m.taylor@gmail.com>
2014-10-01 15:10:30 +0000

    Added some functions tonews-on-time.php

--- news_on_time.php
+++ news_on_time.php
@@ -29,10 +29,51 @@
 			$query = "SELECT * FROM posts WHERE category = 'other events' AND start_date <= CURRENT_DATE() AND end_date >= CURRENT_DATE()";
 			return $this->query($query);
 		}
+		function get_num_movies(){
+			$query = "SELECT COUNT(*) FROM posts WHERE category = 'movies' AND start_date <= CURRENT_DATE() AND end_date >= CURRENT_DATE();";
+			if(!$this->query($query)){
+				return false;
+			}
+			return $this->fetch();
+		}
+		function get_num_parties(){
+			$query = "SELECT COUNT(*) FROM posts WHERE category = 'parties' AND start_date <= CURRENT_DATE() AND end_date >= CURRENT_DATE();";
+			if(!$this->query($query)){
+				return false;
+			}
+			return $this->fetch();
+		}
+		function get_num_foodie(){
+			$query = "SELECT COUNT(*) FROM posts WHERE category = 'foodies' AND start_date <= CURRENT_DATE() AND end_date >= CURRENT_DATE();";
+			if(!$this->query($query)){
+				return false;
+			}
+			return $this->fetch();
+		}
+		function get_num_sports(){
+			$query = "SELECT COUNT(*) FROM posts WHERE category = 'sports' AND start_date <= CURRENT_DATE() AND end_date >= CURRENT_DATE();";
+			if(!$this->query($query)){
+				return false;
+			}
+			return $this->fetch();
+		}
+		function get_num_club_events(){
+			$query = "SELECT COUNT(*) FROM posts WHERE category = 'club events' AND start_date <= CURRENT_DATE() AND end_date >= CURRENT_DATE();";
+			if(!$this->query($query)){
+				return false;
+			}
+			return $this->fetch();
+		}
+		function get_num_other_events(){
+			$query = "SELECT COUNT(*) FROM posts WHERE category = 'other events' AND start_date <= CURRENT_DATE() AND end_date >= CURRENT_DATE();";
+			if(!$this->query($query)){
+				return false;
+			}
+			return $this->fetch();
+		}
 		function add_post($image_path,$description,$category,$start_date,&end_date){
 			$query = "INSERT INTO posts SET image_path='$image_path', description='$description', category='$category', start_date='$start_date', end_date='$end_date'";
 			return $this->query($query);
-
 		}
 	}
 ?>
