df7e09078519d5851fb5ec897f05c050ef1a530a
carl <carl.agbenyega@gmail.com>
2014-10-09 14:49:49 +0000

    few changes to the UI

--- create_account.php
+++ create_account.php
@@ -1,35 +1,35 @@
 <html>
 	<body>
-         <link rel="stylesheet" type="text/css" href="create_account.css"> 
+        <link rel="stylesheet" type="text/css" href="create_account.css">
+        <link rel="stylesheet" type="text/css" href="index.css"> 
+        <div> <!-- class="linkUp" -->
+            <a href="/news-on-time/index.php">Go to Notice board</a>
+        </div>
         <div class="container">
-            <div class="row">
             <div class="col-sm-8">
                 <!--img align="center" src="Ashesi_Logo_Black.png" width="90" height="90"></img-->
-                <img alt="Admin" img align="center" height="95" id="ashesilogo" src="Ashesi_Logo_Black.png" width="90" title="CreateAccount" style="padding-top: 112px;">
-                <h2><text align="center"> Create User Account</h2></text><hr>
+                <img alt="Admin" img align="center" height="95" id="ashesilogo" src="Ashesi_Logo_Black.png" width="90" title="CreateAccount" style="padding-top: 112px; text-align: center">
+                <h2><text align="center"> Create User Account</text></h2>
 
-        <form action="create_account.php" method="post" class="form-horizontal tpad" role="form">
-        <div class="form-group">
+                <form action="create_account.php" method="post" class="form-horizontal tpad" role="form">
+                    <div class="form-group">
+                        First Name<br>
+            			<input type="text" name="firstname"><br>
+                        
+                        Last Name<br>
+                        <input type="text" name="lastname"><br>
+                        
+                        User Name<br>
+                        <input type="text" name="username"><br>
+                        
+                        Password<br>
+                        <input type="password" name="password"><br>
+                        
+                        <input type="submit" name="submit" value="Submit" onclick="popup()">
+                    </div>
+        		</form>
+            </div>
         </div>
-
-            <hr>
-            <br>
-            First Name<br>
-			<input type="text" name="firstname"><br>
-            
-            Last Name<br>
-            <input type="text" name="lastname"><br>
-            
-            User Name<br>
-            <input type="text" name="username"><br>
-            
-            Password<br>
-            <input type="password" name="password"><br>
-            
-            <input type="submit" name="submit" value="Submit" onclick="popup()">
-		</form>
-             
-    
     </body>
     <script>
         function popup(){
